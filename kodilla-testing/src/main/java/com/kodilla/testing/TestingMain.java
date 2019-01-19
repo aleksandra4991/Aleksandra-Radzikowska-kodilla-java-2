a4bccede9da7acfb23223c29864ead6115dc185b
aleksandra4991 <aradzikowska12@gmail.com>
2019-01-19 19:57:11 +0100

    Fourth Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[]args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser=new SimpleUser("theForumUser");
+
+        String result=simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test Ok");
+        }
+        else{
+            System.out.println(("Error!"));
+        }
+
+        Calculator calculator=new Calculator(878,379);
+
+        int resultOfAdding =calculator.add();
+
+        if(resultOfAdding==1258) {
+            System.out.println("Dodawanie działa ok");
+        }
+            else{
+                System.out.println("Dodawanie działa nie ok");
+            }
+
+
+        int resultOfSubtracting =calculator.subtract();
+
+        if(resultOfSubtracting==500) {
+            System.out.println("Odejmowanie działa ok");
+        }
+            else{
+                System.out.println("Odejmowanie działa nie ok");
+            }
     }
 }
